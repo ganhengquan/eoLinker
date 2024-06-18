dd01721e64288e51581a586c7901a0ade66fb214
likun <378186878@qq.com>
2024-06-18 11:52:53 +0800

    init

---
+++ server/RTP/config/eo_config.php
@@ -0,0 +1,31 @@
+<?php
+//Host Address
+defined('DB_URL') or define('DB_URL', '192.168.56.56');
+
+//Host Port, Default mysql is 3306
+defined('DB_PORT') or define('DB_PORT', '3306');
+
+//Database Username
+defined('DB_USER') or define('DB_USER', 'homestead');
+
+//Database Password
+defined('DB_PASSWORD') or define('DB_PASSWORD', 'secret');
+
+//Database Name
+defined('DB_NAME') or define('DB_NAME', 'eolinker_os2');
+
+//Allow Register
+defined('ALLOW_REGISTER') or define('ALLOW_REGISTER', TRUE);
+
+//Allow Update
+defined('ALLOW_UPDATE') or define('ALLOW_UPDATE', FALSE);
+
+//Web Site Name
+defined('WEBSITE_NAME') or define('WEBSITE_NAME', 'EOLINKER AMS OS');
+
+// Database Table header
+defined('DB_TABLE_PREFIXION') or define('DB_TABLE_PREFIXION', 'eo');
+
+//Language
+defined('LANGUAGE') or define ('LANGUAGE', 'zh-cn');
+?>
